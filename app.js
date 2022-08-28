8bba67a953c2ff54ec2ff790a32ab3cef87e3ddb
Raj Bista <rbista@qualityedgar.com>
2022-08-28 15:24:59 -0400

    Create express app

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+    path = require('path'),
+    timeoutTest = require('express-request-timeout-test');
+
+const router = require("./routes");
+const mongoose = require("mongoose");
+const {mongo, server} = require("./config");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+const {test_timeout = 160000 } = server;
+app.use(timeoutTest({timeout: test_timeout, check_auth: false}));
+
+/* MongoDB Connection */
+mongoose.connect(server.url);
+const db = mongoose.connection;
+db.on("connected", () => console.info('MongoDB connection is successful'));
+db.on('error', (err) => console.error("MongoDB connection is failed ", err));
+
+/* Application Routes */
+// app.get('/', (req, res) => res.redirect('/shows'));
+app.use('/', router.shows);
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
+
+module.exports = app;
